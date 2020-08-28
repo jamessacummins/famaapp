84831a47f0b10e155304d08306fe1aa83523378f
jamessacummins <jamessacummins@gmail.com>
2020-08-28 21:26:46 +1000

    green background isn't working?

--- fama/src/pages/feed/feed.page.jsx
+++ fama/src/pages/feed/feed.page.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './feed.styles.css';
 
 export const FeedPage = props => (
-    <div className='FeedPage'>
+    <div className='feed-page'>
         <h1> THIS IS THE FEED PAGE </h1>
     </div>
 );
