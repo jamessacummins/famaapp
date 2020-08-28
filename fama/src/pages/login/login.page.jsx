520cf1484b58bedbf164f149fdbcb887039f6f24
jamessacummins <jamessacummins@gmail.com>
2020-08-29 07:51:07 +1000

    tidying up the login page

--- fama/src/pages/login/login.page.jsx
+++ fama/src/pages/login/login.page.jsx
@@ -6,7 +6,9 @@ export const LoginPage = props => (
     <div className='login-page'>
         <div className='login-page-container'>
             <img className='logo' src={logo} alt='logo'></img>
-            <div className='break'></div>
+            <input className = 'username' type='text' placeholder='Enter Your Username'></input>
+            <input className='password' type='password' placeholder='Enter Your Password'></input>
+            <p className='reset-text'><b>Reset Your Password</b></p>
             <button className='login-button' onClick={props.updatePageToFeed}><b>Login</b></button>
         </div>
     </div>
